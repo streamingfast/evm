6c21ced9a0cbdbc09be3eaece03a1a3db7ddd639
Matthieu Vachon <matt@streamingfast.io>
2026-03-28 04:14:01 +0000

    Enable system call to be traced by inspector
    When using ExEx to replay a full block including the pre/post execution steps, system calls weren't inspected before if inspector was enabled.
    
    One thing I'm unusure of the impact is about other inspector out there, would they be affected?

--- crates/evm/src/eth/mod.rs
+++ crates/evm/src/eth/mod.rs
@@ -19,7 +19,8 @@ use revm::{
     interpreter::{interpreter::EthInterpreter, InterpreterResult},
     precompile::{PrecompileSpecId, Precompiles},
     primitives::hardfork::SpecId,
-    Context, ExecuteEvm, InspectEvm, Inspector, MainBuilder, MainContext, SystemCallEvm,
+    Context, ExecuteEvm, InspectEvm, InspectSystemCallEvm, Inspector, MainBuilder, MainContext,
+    SystemCallEvm,
 };
 
 mod block;
@@ -235,7 +236,12 @@ where
         contract: Address,
         data: Bytes,
     ) -> Result<ResultAndState<Self::HaltReason>, Self::Error> {
-        self.inner.system_call_with_caller(caller, contract, data)
+        if self.inspect {
+            self.inner
+                .inspect_system_call_with_caller(caller, contract, data)
+        } else {
+            self.inner.system_call_with_caller(caller, contract, data)
+        }
     }
 
     fn finish(self) -> (Self::DB, EvmEnv<Self::Spec>) {
